c1ebe6ca1620c552a0cdcf4230e0a9de129284a7
Daniel Vlasits <dvlasits@gmail.com>
2021-10-24 01:01:10 +0100

    Updated with sequences for efficiency

--- Graph.hs
+++ Graph.hs
@@ -4,6 +4,9 @@ import qualified Data.Map as M
 import Data.Set (Set)
 import qualified Data.Set as S
 import Data.List
+import Data.Sequence (Seq, (|>), (><), (<|))
+import qualified Data.Sequence as Sq
+import qualified Data.Foldable as F
 
 
 
@@ -21,24 +24,26 @@ createUniDirectional = createAdjList . fixing
         fixing x = x >>= (\(x,y) -> [(x,y), (y,x)])
 
 
+add g (a,b) = M.insertWith (++) (a,[b])
+
 degree g x = length $ g ? x
 
 dfs g x = dfs' g x S.empty
 
-dfs' g x s = dfs'' [x] s []
+dfs' g x s = dfs'' [x] s Sq.empty
      where
          dfs'' [] s acc = (s, acc)
          dfs'' (x:xs) s acc
              | x `S.member` s = dfs'' xs s acc
              | otherwise = 
                  let (outSet, outList) = dfs'' (g ? x) (S.insert x s) acc
-                 in dfs'' xs outSet (outList ++ [x]) 
+                 in dfs'' xs outSet (x <| outList)
 
 getNodes = map fst . M.toList
 
-topoSort :: (Ord k) => M.Map k [k] -> [k]
-topoSort g = reverse . fst . foldr doStuff ([],S.empty) . getNodes $ g
+topoSort :: (Ord k) => M.Map k [k] -> Seq k
+topoSort g = fst . foldr doStuff (Sq.empty,S.empty) . getNodes $ g
     where 
         doStuff item (topoed,s) =
             let (s2, items) = dfs' g item s
-            in (topoed ++ items, s2)
+            in (items >< topoed, s2)
